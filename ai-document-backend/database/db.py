e5646af95f3ac0d4ed94e5970ba90cbfc7834877
satyanarayana <SATYANARAYANAPASUPUNURI@GMAIL.COM>
2026-07-09 22:20:47 +0530

    database reset

--- ai-document-backend/database/db.py
+++ ai-document-backend/database/db.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("postgresql://ai_pdf_assistant_90nk_user:AjDJiGup4EpDEOqRAiOeKolHPkYpOlRK@dpg-d97shn67r5hc73dbs1gg-a.virginia-postgres.render.com/ai_pdf_assistant_90nk")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
