c3ffe30530082daebc8a191d6be467b370ba3cd7
satyanarayana <SATYANARAYANAPASUPUNURI@GMAIL.COM>
2026-07-10 17:44:31 +0530

    railway deploy database setting

--- ai-document-backend/database/db.py
+++ ai-document-backend/database/db.py
@@ -7,7 +7,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not found!")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
